facd67b2dd29e271ea1cf6d1a57b4993333b13ae
Mikeil Akovity (1iekim) <mikeil98@mail.ru>
2023-03-04 18:54:00 +0300

    feat: add sort posts

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MuInput";
+import MySelect from "./components/UI/select/MySelect";
 import "./styles/App.css";
 
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "Javascript", body: "Description" },
-    { id: 2, title: "Javascript 1", body: "Description" },
-    { id: 3, title: "Javascript 2", body: "Description" },
+    { id: 2, title: "aaaa", body: "xxxxx" },
+    { id: 3, title: "xxxx", body: "aaaaa" },
   ]);
+
+  const [selectedSort, setSelectedSort] = useState("");
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -22,10 +25,32 @@ function App() {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a,b)=>a[sort].localeCompare(b[sort])))
+  };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      <hr style={{ margin: "15px" }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка по"
+          options={[
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
+          ]}
+        />
+      </div>
+
+      {posts.length !== 0 ? (
+        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      ) : (
+        <h1 style={{ textAlign: "center" }}>Посты не найдены!</h1>
+      )}
     </div>
   );
 }
