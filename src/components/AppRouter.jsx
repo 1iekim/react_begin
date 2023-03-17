7f6c04822d53042b9c170028a38d4ea445e4cbe8
Mikeil Akovity (1iekim) <mikeil98@mail.ru>
2023-03-17 23:56:14 +0300

    feat: use fetch for get post item

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import About from "../pages/About";
 import ErrorPage from "../pages/ErrorPage";
+import PostIdPage from "../pages/PostIdPage";
 import Posts from "../pages/Posts";
 
 const AppRouter = () => {
@@ -9,7 +10,8 @@ const AppRouter = () => {
     <Routes>
       <Route path="/about" element={<About />} />
       <Route path="/" element={<About />} />
-      <Route path="/posts" element={<Posts />} />
+      <Route exact path="/posts" element={<Posts />} />
+      <Route exact path="/posts/:id" element={<PostIdPage />} />
       <Route path="*" index element={<ErrorPage />} />
     </Routes>
   );
